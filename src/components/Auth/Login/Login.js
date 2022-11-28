fea94bf7e158e09b2d27e77ae0f2221a3d23fe50
Kazi Arif <coderkaziarif@gmail.com>
2022-11-28 09:02:59 +0600

    Login form validation done

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,21 +1,48 @@
-import React from 'react';
-import { Avatar, Button, FormControlLabel, Grid, Paper, TextField, Checkbox, Typography, Link  } from '@mui/material';
+import React, { useState } from 'react';
+import { Avatar, Button, FormControlLabel, Grid, Paper, TextField, Checkbox, Typography, Link, Alert  } from '@mui/material';
 // import LockIcon from '@mui/icons-material/Lock';
 import PersonIcon from '@mui/icons-material/Person';
+import {NavLink, useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
-   const paperStyle={padding:20, width:350, height:'74vh', margin:"0 auto", }
+   const navigate = useNavigate();
+   const paperStyle={padding:20, width:350, height:'74vh', margin:"0 auto", };
+
+   const [error, setError] = useState({
+      status: false,
+      msg: "",
+      type: "",
+   });
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      const data = new FormData (e.currentTarget);
+      const actualData = {
+         email : data.get('email'),
+         password: data.get('password'),
+      }
+      if(actualData.email && actualData.password){
+         setError({status:true, msg:"Login Successfully.", type:"success"})
+         console.log(actualData);
+         document.getElementById('login_form').reset();
+         setTimeout(()=>{
+            navigate('/home');
+         },3000)
+      } else{
+         setError({ status:true, msg:"All Fields are Required !", type:'error'})
+      }    
+   }
+
 
    return (
-      <Grid>
+      <Grid component='form' noValidate id='login_form' onSubmit = {handleSubmit}>
          <Paper style={paperStyle}>
             <Grid align="center" sx={{marginBottom:"15px"}}>
                <Avatar sx={{backgroundColor:"purple"}}><PersonIcon/></Avatar>
                <Typography variant='h5' color='secondary'>Login </Typography> 
             </Grid>
-               <TextField variant="standard" color='secondary' label="Username"  type='text' placeholder='Enter your Name'  fullWidth required></TextField>
-               <TextField  variant="standard" color='secondary' label="Password"  type='password' placeholder='Enter your password' fullWidth required>
+               <TextField variant="standard" color='secondary' label="UserEmail"  type='text' placeholder='Enter your Email' name="email"  fullWidth required></TextField>
+               <TextField  variant="standard" color='secondary' label="Password"  type='password' placeholder='Enter your password' name = "password" fullWidth required>
                </TextField>
                <FormControlLabel sx={{marginTop:"8px"}}
                   control={
@@ -29,7 +56,8 @@ const Login = () => {
                </Typography>   
                <Typography>
                   Do you have an account ?
-                  <Link href="#"> Sign up </Link>
+                  <NavLink to ="signup"> Sign up </NavLink>
+                  {error.status ? <Alert severity={error.type}>{error.msg}</Alert> : ""}
                </Typography>      
          </Paper>
          
--- src/components/Auth/Signup/Signup.js
+++ src/components/Auth/Signup/Signup.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import { Avatar, Button, Checkbox, FormControlLabel, Grid, Paper, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Alert, Avatar, Button, Checkbox, FormControlLabel, Grid, Paper, TextField, Typography } from '@mui/material';
 // import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import { useNavigate } from 'react-router-dom';
 
 
 const Signup = () => {
    const paperStyle ={padding: "20px 20px",margin:"0 auto", width:350, height:'74vh'};
+   // *Form validation
+   const navigate = useNavigate();
+   const [error, setError] = useState({
+      status: false,
+      msg: "",
+      type: "",
+   });
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      const data = new FormData (e.currentTarget);
+      const actualData = {
+         name : data.get('name'),
+         email : data.get('email'),
+         password: data.get('password'),
+         confirm_Password: data.get('confirm_Password'),
+         tc:data.get('tc')
+      }
+      if(actualData.name && actualData.email && actualData.password && actualData.confirm_Password && actualData.tc !== null){
+         setError({status:true, msg:"Account Create Successfully.", type:"success"})
+         console.log(actualData);
+         document.getElementById('signup_form').reset();
+         setTimeout(()=>{
+            // navigate('/home');
+         },3000)
+      } else{
+         setError({ status:true, msg:"All Fields are Required !", type:'error'})
+      }    
+   }
 
    return (
       <>
-      <Grid>
+      <Grid component="form" id="signup_form" noValidate onSubmit={handleSubmit}>
          <Paper style={paperStyle}>
             <Grid align="center">
                <Avatar sx={{backgroundColor:"purple"}}><PersonAddIcon/></Avatar>
@@ -27,13 +56,14 @@ const Signup = () => {
 
                <TextField variant ="standard" type="password" name="password" label='Password' placeholder="Enter your password" fullWidth required color='secondary'></TextField>
 
-               <TextField variant ="standard" type="password" name="cPassword"  label='Confirm password' placeholder="Confirm password" fullWidth required color='secondary'></TextField>
+               <TextField variant ="standard" type="password" name="confirm_Password"  label='Confirm password' placeholder="Confirm password" fullWidth required color='secondary'></TextField>
 
-               <FormControlLabel sx={{marginTop:"8px"}} control={<Checkbox name="checkedA" color="secondary" size="small"/>}
+               <FormControlLabel sx={{marginTop:"8px"}} control={<Checkbox value="agree" name="tc" id="tc" color="secondary" size="small"/>}
                label="I accept the terms and condition."/>
 
-               <Button type='submit' variant='contained' color='secondary' fullWidth style={{margin:"8px 0"}}>Sign up</Button>
-            </form>            
+               <Button type='submit' onClick={(e)=> console.log('clicked')} variant='contained' color='secondary' fullWidth style={{margin:"8px 0"}}>Sign up</Button>
+               {error.status ? <Alert severity={error.type}>{error.msg}</Alert> : ""}
+            </form>             
          </Paper>
       </Grid>
        
